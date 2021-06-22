3c41b9b2df395ede7fc1634c9dbbee8327fac10c
anurag-shukla8004 <79192104+anurag-shukla8004@users.noreply.github.com>
2021-06-22 20:03:36 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './component/Greet'
 import Welcome from './component/Welcome'
+import Counter from './component/Counter'
 
 
 class App extends Component{
   render(){
     return(
       <div className='App'>
+        <Counter/>
         <Greet/>
        <Welcome/>
       </div>
