62a3a77cbd94787120bfcce108c4c0fb34114c6f
anurag-shukla8004 <79192104+anurag-shukla8004@users.noreply.github.com>
2021-06-25 08:06:38 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {Component} from 'react'
-import User from './User'
+import React from 'react'
+import './App.css'
 
-export default function App() {
+function App() {
+
+  function getFormData(e){
+     e.preventDefault()
+  }
   return (
-   <div className="App">
-     <h1>hooo</h1>
-     <User/>
-   </div>
-  );
+    <div className="App">
+      <h1>Handle form in react</h1>
+      <form onSubmit={getFormData}>
+        <input type="text" placeholder="Enter name" /> <br/> <br/>
+        <select>
+          <option>Select Option</option>
+          <option>Marvels</option>
+          <option>DC</option>
+        </select> <br/> <br/>
+        <input type="checkbox"/>  <span>Term And Condition </span>
+        <br/> <br/>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
 }
+
+export default App
+
 
 
